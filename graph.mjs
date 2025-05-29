badea257371bb9ed196b3d2366c9b227bf2a99b4
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-05-29 19:22:02 -0400

    Write nodes for chessboard

--- graph.mjs
+++ graph.mjs
@@ -1,10 +1,18 @@
+class Node {
+    constructor(x, y) {
+        this.xCoordinate = x;
+        this.yCoordinate = y;
+    }
+}
+
 class Graph {
     constructor() {
         this.nodes = [];
     }
 
     addNode(x, y) {
-        this.nodes[x, y] = [x, y];
+        const newNode = new Node(x, y);
+        this.nodes.push(newNode);
     }
 }
 
