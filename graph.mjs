25451e816d163905b30cef51b8f2c9ce43a52576
bradgerb <77023448+bradgerb@users.noreply.github.com>
2025-05-30 13:53:53 -0400

    Write add edges

--- graph.mjs
+++ graph.mjs
@@ -1,7 +1,11 @@
+const rows = 8;
+const columns = 8;
+
 class Node {
     constructor(x, y) {
         this.xCoordinate = x;
         this.yCoordinate = y;
+        this.edges = [];
     }
 }
 
@@ -14,6 +18,35 @@ class Graph {
         const newNode = new Node(x, y);
         this.nodes.push(newNode);
     }
+
+    addEdge([sourceX, sourceY], [destinationX, destinationY]) {
+        let sourceNode = coordinatesToNode([sourceX, sourceY]);
+        let destinationNode = coordinatesToNode([destinationX, destinationY]);
+
+        if (sourceX >= 0 && sourceX <= rows - 1 &&
+            sourceY >= 0 && sourceY <= columns - 1 &&
+            destinationX >= 0 && destinationX <= rows - 1 &&
+            destinationY >= 0 && destinationY <= columns - 1) {
+                this.nodes[sourceNode].edges.push(destinationNode);
+                this.nodes[destinationNode].edges.push(sourceNode);
+            }
+    }
 }
 
-export default Graph
+const coordinatesToNode = (array)=> {
+    let x = array[0];
+    let y = array[1];
+    let node = x + (rows * y);
+
+    return node
+}
+
+const nodeToCoordinates = (node)=> {
+    let x = node % rows;
+    let y = Math.floor(node / rows);
+    let array = [x, y];
+
+    return array
+}
+
+export { rows, columns, Graph }
--- main.mjs
+++ main.mjs
@@ -1,13 +1,21 @@
-import Graph from "./graph.mjs";
+import {rows, columns, Graph } from "./graph.mjs";
 
 const chessboard = new Graph();
 
-console.table(chessboard);
-
-for(let x = 0; x < 8; x++) {
-    for(let y = 0; y < 8; y++) {
+for(let y = 0; y < columns; y++) {
+    for(let x = 0; x < rows; x++) {
         chessboard.addNode(x, y);
     }
 }
 
+for(let y = 0; y < columns; y++) {
+    for(let x = 0; x < rows; x++) {
+        chessboard.addEdge([x, y],[x + 1, y + 2]);
+        chessboard.addEdge([x, y],[x + 2, y + 1]);
+        chessboard.addEdge([x, y],[x - 1, y + 2]);
+        chessboard.addEdge([x, y],[x - 2, y + 1]);
+    }
+}
+
 console.log(chessboard);
+console.log(chessboard.nodes[28].edges);
